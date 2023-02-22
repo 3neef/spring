133195f9ec0106c085c896412e6133e420750ef9
mazin hafiz <mazin.hafiz@nctr.sd>
2023-02-22 09:17:49 +0200

    :tada: initializing upsales

---
+++ upsales/src/main/java/com/upsales/upsales/UpsalesApplication.java
@@ -0,0 +1,13 @@
+package com.upsales.upsales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpsalesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpsalesApplication.class, args);
+	}
+
+}
---
+++ upsales/src/test/java/com/upsales/upsales/UpsalesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upsales.upsales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpsalesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
